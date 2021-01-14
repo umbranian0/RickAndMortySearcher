31053b315a936a169d0c8668711e5fec01de626d
umbranian0 <vasiletimotin@outlook.pt>
2021-01-14 22:32:58 +0000

    Simple rick and morty character searcher with NextJs app

---
+++ merchendising/pages/index.js
@@ -0,0 +1,148 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import React, { useEffect, useState } from "react";
+
+const defaultEndpoint = `https://rickandmortyapi.com/api/character/`;
+
+export async function getServerSideProps() {
+  const res = await fetch(defaultEndpoint)
+  const data = await res.json();
+  return {
+    props: {
+      data
+    }
+  }
+}
+
+
+
+export default function Home({data}) {
+
+  const { info, results: defaultResults = [] } = data;
+  const [results, updateResults] = useState(defaultResults);
+  
+  const [page, updatePage] = useState({
+    ...info,
+    current: defaultEndpoint
+  });
+//----------------  
+  //load more images
+  const { current } = page;
+
+  useEffect(() => {
+    if ( current === defaultEndpoint ) return;
+  
+    async function request() {
+      const res = await fetch(current)
+      const nextData = await res.json();
+  
+      updatePage({
+        current,
+        ...nextData.info
+      });
+  
+      if ( !nextData.info?.prev ) {
+        updateResults(nextData.results);
+        return;
+      }
+  
+      updateResults(prev => {
+        return [
+          ...prev,
+          ...nextData.results
+        ]
+      });
+    }
+  
+    request();
+  }, [current]);
+//----------------  
+
+  function handleLoadMore() {
+    updatePage(prev => {
+      return {
+        ...prev,
+        current: page?.next
+      }
+    });
+  }
+ //----------------   
+  //Search method
+  function handleOnSubmitSearch(e) {
+    e.preventDefault();
+  
+    const { currentTarget = {} } = e;
+    const fields = Array.from(currentTarget?.elements);
+    const fieldQuery = fields.find(field => field.name === 'query');
+  
+    const value = fieldQuery.value || '';
+    const endpoint = `https://rickandmortyapi.com/api/character/?name=${value}`;
+  
+    updatePage({
+      current: endpoint
+    });
+  }
+//------------------  
+  console.log('data', data);
+  
+  return (
+ 
+    <div className={styles.container}>
+      <Head>
+        <title>Rick and Morty</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+         Wullabulla morty
+        </h1>
+
+        <p className={styles.description}>
+        <h3>
+         Rick and morty Character Wiki
+        </h3>
+          <form className={styles.search} onSubmit={handleOnSubmitSearch}>
+          <input name="query" type="search" />
+          <button>Search</button>
+          </form>
+        </p>
+
+        <div className={styles.grid}>
+          
+          <ul className={styles.grid}>
+          {results.map(result => {
+            const { id, name, image } = result;
+
+            return (
+              <li key={id} className={styles.card}>
+                <a href="#">
+                  <img src={image} alt={`${name} Thumbnail`} />
+                  <h3>{ name }</h3>
+                </a>
+              </li>
+            )
+          })
+          }
+        </ul>
+
+        </div>
+        <p>
+        <button onClick={handleLoadMore}>Load More</button>
+        </p>
+
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
+    </div>
+  )
+}
